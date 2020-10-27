4d2b839c2a9e3e492262de3f76781224718c3508
lrmendes98 <lrmmendes98@gmail.com>
2020-10-27 10:42:03 +0000

    Added input handling, new header file folder and aux files

---
+++ Trabalho 1/include/aux.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string.h>
+#include <stdlib.h>
+
+char* stringAdd(const char *s1, const char *s2);
--- Trabalho 1/src/main.c
+++ Trabalho 1/src/main.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include "aux.h"
+
+int main(int argc, char *argv[])
+{
+    char operation[100] = "";
+    char port[100] = "";
 
 
-int main(int argc, char *argv[]) {
+    // Handle program arguments
+    if (argc > 3 || ((strcmp("read", argv[1]) != 0) && (strcmp("write", argv[1]) != 0))) {
+        printf("./rcom <read|write> <port> \n");
+        return 1;
+    }    
+    else {
+        if (strcmp("read", argv[1]) == 0)
+		    strcpy(operation, "TRANSMITTER");
+        if (strcmp("write", argv[1]) == 0)
+            strcpy(operation, "RECEIVER");
 
-    printf("funciona\n");
-    
+        strcpy(port, argv[2]);
+    }
+
+    printf("Port: %s", port);
+    printf("operation: %s", operation);
+    printf("\n");
+
     return 0;
 }
