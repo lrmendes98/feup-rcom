262fee3fdbf686be531da934780b7841c7838628
lrmendes98 <lrmmendes98@gmail.com>
2020-12-09 20:09:03 +0000

    added Trabalho_2

---
+++ Trabalho_2/src/main.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("Hello\n");
+
+    return 0;
+}
